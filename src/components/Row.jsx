b6c814afd756c483844c5f5df9894e81d35b2c15
itsJae <limwoojaedev@naver.com>
2024-11-16 13:35:07 +0000

    Added swiper

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,6 +2,12 @@ import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import MovieModal from "./MovieModal";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function Row({ title, id, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
@@ -25,44 +31,55 @@ function Row({ title, id, fetchUrl, isLargeRow }) {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div
-          className="slider__arrow-left"
-          onClick={() => {
-            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{"<"}</span>
-        </div>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation
+        pagination={{ clickable: true }}
+      >
         <div id={id} className="row__posters">
           {movies && movies.length > 0 ? (
             movies.map((movie) => {
               return (
-                <img
-                  key={movie.id}
-                  style={{ padding: "25px 0" }}
-                  className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                  src={`https://image.tmdb.org/t/p/original/${
-                    isLargeRow ? movie.poster_path : movie.backdrop_path
-                  }`}
-                  alt={movie.name || movie.title}
-                  onClick={() => handleClick(movie)}
-                />
+                <SwiperSlide>
+                  <img
+                    key={movie.id}
+                    style={{ padding: "25px 0" }}
+                    className={`row__poster ${
+                      isLargeRow && "row__posterLarge"
+                    }`}
+                    src={`https://image.tmdb.org/t/p/original/${
+                      isLargeRow ? movie.poster_path : movie.backdrop_path
+                    }`}
+                    alt={movie.name || movie.title}
+                    onClick={() => handleClick(movie)}
+                  />
+                </SwiperSlide>
               );
             })
           ) : (
             <p>No movies available.</p>
           )}
         </div>
-        <div
-          className="slider__arrow-right"
-          onClick={() => {
-            document.getElementById(id).scrollLeft += window.innerWidth + 80;
-          }}
-        >
-          <span className="arrow">{">"}</span>
-        </div>
-      </div>
+      </Swiper>
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -9,18 +9,17 @@ function DetailPage() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(`/movie/${movieId}`);
-      setMovie(request);
+      setMovie(request.data);
     }
     fetchData();
   }, [movieId]);
-
-  if (!movie.backdrop_path) return <div>...loading</div>;
 
   return (
     <section>
       <img
         className="modal__poster-img"
         src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="poster"
       />
     </section>
   );

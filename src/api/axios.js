9388b1c36bfa7f5324b72d4bc781e00c9ed82bb4
itsJae <limwoojaedev@naver.com>
2024-11-19 13:00:46 +0000

    Hid api key

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "e5b3472b2645e71aeb6e0fb6518ab1f7",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "en-US",
   },
 });
